84655c23052d63b137f5fd5e7bb628836cab8c0e
Carlos Yanes <alu0101430720@ull.edu.es>
2026-02-27 01:26:22 +0000

    Actualización de structure

--- structure/config.py
+++ structure/config.py
@@ -4,7 +4,7 @@
 REPO_DIR   = "/content/Pr2-Visualizacion"
 REPO_OWNER = "alu0101430720"
 REPO_NAME  = "Pr2-Visualizacion"
-GIT_BRANCH = "practica3/data-checks"
+GIT_BRANCH = "practica3_refactorizado"
 GIT_EMAIL  = "alu0101430720@ull.edu.es"
 GIT_NAME   = "Carlos Yanes"
 
